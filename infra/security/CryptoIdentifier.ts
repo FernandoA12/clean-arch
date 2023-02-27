78e7fd9ceeef64efdaae911a82ae5395bd5f88ac
Fernando Souza <fernandoantoniocardosodesouza@gmail.com>
2023-02-27 15:02:33 -0300

    feat: crypto identifier

---
+++ infra/security/CryptoIdentifier.ts
@@ -0,0 +1,7 @@
+import { randomUUID } from "crypto";
+import { Identifier } from "./Identifier";
+export class CryptoIdentifier implements Identifier {
+  createId(): string {
+    return randomUUID().toString();
+  }
+}
